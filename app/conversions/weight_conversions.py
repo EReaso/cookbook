cd9de6db527dec8600c0629a894285e41770c723
iRaccoon13 <136942618+iRaccoon13@users.noreply.github.com>
2026-01-25 07:23:19 -0800

    Removed unused conversion files

--- app/conversions/weight_conversions.py
+++
@@ -1,6 +0,0 @@
-from bs4 import BeautifulSoup
-import requests
-
-url = "https://www.kingarthurbaking.com/learn/ingredient-weight-chart"  # Replace with your target URL
-response = requests.get(url)
-soup = BeautifulSoup(response.text, 'html.parser')
